63adce87cc56431ba5b6a5ba30265d67309b8161
HuseyiN Gokdemir <huseyingokdemir@msn.com>
2025-08-01 22:19:15 +0300

    Infrastructure Done

---
+++ IKARUSWEB.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKARUSWEB.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        // Extra user properties burada
+    }
+}
---
+++ IKARUSWEB.Infrastructure/Persistence/Configurations/TenantConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IKARUSWEB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKARUSWEB.Infrastructure.Persistence.Configurations
+{
+    public class TenantConfiguration : IEntityTypeConfiguration<Tenant>
+    {
+        public void Configure(EntityTypeBuilder<Tenant> builder)
+        {
+            builder.ToTable("Tenants");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.TenantIdentifier).IsRequired().HasMaxLength(50);
+            
+        }
+    }
+}
